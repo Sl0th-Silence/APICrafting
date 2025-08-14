ddef436dcbc3fdb3611d5363147c26f1ad992207
Jay Mills <73972373+Sl0th-Silence@users.noreply.github.com>
2025-08-14 10:41:28 -0400

    Removed testing stuff

--- server.js
+++ server.js
@@ -6,30 +6,13 @@ const server = express(); //Server
 let port = 3000;
 
 //Paths
-var paths = {
-    home: "home",
-    info: "info",
-    contact: "contact"
-};
-//Array of people objects
-var people = [
-    {id: 1, name: "Phil"},
-    {id: 2, name: "Joe"},
-    {id: 3, name: "Sally"}
-];
 
 // Middleware
-
 
 //Server Paths
 server.get( '/', (request, response) => {
     response.send("Welcome holmes!");
 });
-
-server.get('/people', (request, response) => {
-    response.json(people);
-});
-
 
 //Debugging
 server.listen(port, () => {
